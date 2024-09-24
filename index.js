1ff611f413521e0f9504bf4844ca4ecf10a93573
Stephen <paf1566@patientadvocate.org>
2024-09-24 20:44:14 +0530

    v1

--- index.js
+++ index.js
@@ -13,6 +13,7 @@ const axios = require("axios")
 const Multitenancy = require("supertokens-node/recipe/multitenancy");
 const Tenant = require('supertokens-node/recipe/multitenancy');
 const { EmailPassword } = require("supertokens-node/recipe/emailpassword");
+const cookieParser = require('cookie-parser');
 
 const { middleware, errorHandler, SessionRequest } = pkg
 
@@ -26,6 +27,9 @@ const dotEnv = process.env
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+// Use cookie-parser middleware
+app.use(cookieParser());
+
 app.use(
     cors({
         origin: dotEnv.WEBSITE_DOMAIN,
@@ -36,6 +40,12 @@ app.use(
 );
 
 app.use((req, res, next) => {
+    res.cookie('myGlobalCookie', 'cookieValue', {
+        httpOnly: true, // Makes the cookie inaccessible to JavaScript
+        secure: true, // Set to true in production
+        // maxAge: 1000 * 60 * 60 * 24, // 1 day
+        sameSite: 'None', // CSRF protection
+    });
     req.tenantId = "public"; // Add the tenant ID to the request object
     next();
 });
@@ -69,6 +79,7 @@ app.post("/getJwtToken", verifySession(), async (req, res) => {
 })
 
 app.get('/set-cookie', (req, res) => {
+    console.log('checking cookie')
     res.cookie('myCookie', 'value', {
         sameSite: 'None', // or 'Lax' / 'Strict'secure: true,     // Ensure to set this if using SameSite=None    });     res.send('Cookie set'); });
     })
